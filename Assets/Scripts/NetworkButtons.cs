4bf1a1929529a0a57fb02fc150b2736f3615579e
pipi0411 <20a41010217@students.hou.edu.vn>
2026-04-08 16:17:33 +0700

    update UI lobby and main

---
+++ Assets/Scripts/NetworkButtons.cs
@@ -0,0 +1,15 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkButtons : MonoBehaviour
+{
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
+    }
+
+    public void StartClient()
+    {
+        NetworkManager.Singleton.StartClient();
+    }
+}
