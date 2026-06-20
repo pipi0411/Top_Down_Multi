8e1ab9c470a42dd43e99d7143bf0b79b4f63c032
QuagHuyomi <huyluongthien@gmail.com>
2026-06-20 16:56:04 +0700

    Player Panel UI

---
+++ Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Reference")]
+   [SerializeField] private PlayerConfig playerConfig;
+   [SerializeField] private
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
